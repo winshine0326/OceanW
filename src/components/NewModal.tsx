19981bbc6e6d42466c60df8a96287a7dc02df751
winshine0326 <shane10340@gmail.com>
2025-05-06 17:08:47 +0900

    feat :: text area 추가

--- src/components/NewModal.tsx
+++ src/components/NewModal.tsx
@@ -6,10 +6,64 @@ const modalContainer: React.CSSProperties = {
     top:'0',
 }
 
+const writingSection:React.CSSProperties = {
+    width:'38%',
+    height:'95%',
+    backgroundColor:'#1E1E1E',
+    position:'absolute',
+    left:'15%',
+    top:'2.5%',
+    zIndex:'999',
+    borderRadius:'10px',
+    padding:'0 20px'
+}
+
+const titleStyle:React.CSSProperties = {
+    width:'100%',
+    height: '50px',
+    background : 'none',
+    color:'white',
+    outline:'none',
+    border:'none',
+    fontSize:'xx-large',
+    margin:'10px 10px'
+}
+
+const previewSection:React.CSSProperties = {
+    width:'38%',
+    height:'95%',
+    backgroundColor:'#1E1E1E',
+    position:'absolute',
+    left:'57.5%',
+    top:'2.5%',
+    zIndex:'999',
+    borderRadius:'10px',
+    padding:'0 20px'
+}
+
+const contentStyle: React.CSSProperties = {
+    width: '100%',
+    height: '80%',
+    margin: '5% 1.5%',
+    background: 'none',
+    color: 'white',
+    outline: 'none',
+    border: 'none',
+    fontSize: 'large',
+};
+
 const NewModal: React.FC = () => {
     return (
         <div style={modalContainer}>
-
+            <section style={writingSection}>
+                <input type="text" style={titleStyle} placeholder="제목"/>
+                <hr />
+                <textarea style={contentStyle} placeholder="내용을 입력하세요" />
+            </section>
+            <section style={previewSection}>
+                <input type="text" style={titleStyle} placeholder="제목" readOnly/>
+                <hr />
+            </section>
         </div>
     )
 }
