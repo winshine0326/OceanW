ea016d2d19a55ef936b709a1afa9715a67a93981
winshine0326 <shane10340@gmail.com>
2025-05-06 16:31:45 +0900

    feat :: newArticle page 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './global.css'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
 import Home from './pages/Home/Home'
+import NewArticle from './pages/NewArticle/NewArticle'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route element={<Layout/>}>
           <Route path='/' element={<Home/>}/>
+          <Route path='/new' element={<NewArticle/>}/>
         </Route>
       </Routes>
     </>
